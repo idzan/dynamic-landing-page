0ad04d570cb6e35b229a1e7e52eb8bc77e5721b2
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-19 14:12:24 +0100

    Update app.js
    Coloring fix

--- app.js
+++ app.js
@@ -36,10 +36,12 @@ function setBg() {
 
     if (currentHours < 11 && currentHours > 7) {
         document.body.style.background = "url('../img/morning.jpg')";
+        document.body.style.color = "white";
     } else if (currentHours < 17 && currentHours > 11) {
         document.body.style.background = "url('../img/day.jpg')";
     } else if (currentHours < 20 && currentHours > 17) {
         document.body.style.background = "url('../img/afternoon.jpg')";
+        document.body.style.color = "white";
     } else {
         document.body.style.background = "url('../img/night.jpg')";
         document.body.style.color = "white";
