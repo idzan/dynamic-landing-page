c2b50f1115b4b2e7a89a04f7352361d6b668fd54
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-19 01:18:25 +0100

    Basic layout and styling
    Plus, constants for using in app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const time = document.getElementById('time');
+const greeting = document.getElementById('greetings');
+
+const showAmPm = false;
+
+function showTime () {
+    let today = new Date();
+    let hour = today.getHours();
+    let minuutes = today.getMinutes();
+}
