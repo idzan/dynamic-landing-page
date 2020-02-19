6b06f5d8dc2b98b25317094a18bb0833d4f85b32
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-19 01:38:52 +0100

    basic app works
    to-do: move all constants out and fix date names to be full date name in English

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const time = document.getElementById('time');
+const date = document.getElementById('date');
+const weekday = document.getElementById('weekday');
 const greeting = document.getElementById('greetings');
 
-const showAmPm = false;
+const today = new Date();
+const todayDate = new Date();
+const weekdays = ["Sun","Mon","Tue","Wen","Thu","Fri","Sat"];
+
 
 function showTime () {
-    let today = new Date();
     let hour = today.getHours();
-    let minuutes = today.getMinutes();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    time.innerHTML = `${addZeros(hour)}:${addZeros(minutes)}:${addZeros(seconds)}`;
 }
+
+function showDate () {
+    let dates = todayDate.getDate();
+    let month = todayDate.getMonth()+1;
+    let year = todayDate.getFullYear();
+
+    date.innerHTML = `${addZeros(dates)}.${addZeros(month)}.${year}.`;
+    weekday.innerHTML = `${weekdays[todayDate.getDay()]}`;
+}
+
+function addZeros(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+showDate();
+showTime();
